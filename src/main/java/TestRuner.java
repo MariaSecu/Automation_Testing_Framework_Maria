2cc0477df3cba1ba2cb3cdae4c623e6afd0a9a15
maria <maria.s3cu@gmail.com>
2023-02-15 13:28:21 +0200

    Creating a new driver and open Chrome website.

--- src/main/java/TestRuner.java
+++ src/main/java/TestRuner.java
@@ -1,5 +1,10 @@
+import managers.WebDriverManager;
+
 public class TestRuner {
     public static void main(String[] args) {
 
+        WebDriverManager webDriverManager = new WebDriverManager("CHROME");
+        webDriverManager.getDriver().get("https:/www.google.md//");
+        webDriverManager.closeDriver();
     }
 }
